d1f52bc31aebb6eb12f18f88b6077a82cb28f937
Tomas Escamez <tomase@gmail.com>
2023-05-08 20:33:50 +0200

    Add grid to user profile

--- src/routes/user-profile/user-profile.component.tsx
+++ src/routes/user-profile/user-profile.component.tsx
@@ -1,7 +1,36 @@
-import { Box } from "@mui/material";
+import { Grid, Card, Box, Paper } from "@mui/material";
 
 const UserProfile: React.FC = () => {
-  return <Box>This is the user profile</Box>;
+  return (
+    <Box sx={{ width: "100%", paddingTop: "5px" }}>
+      <Grid container direction="column" spacing={2}>
+        <Grid item xs={12}>
+          <Card
+            variant="outlined"
+            sx={{
+              height: 200,
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Card
+            variant="outlined"
+            sx={{
+              height: 200,
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Card
+            variant="outlined"
+            sx={{
+              height: 200,
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default UserProfile;
